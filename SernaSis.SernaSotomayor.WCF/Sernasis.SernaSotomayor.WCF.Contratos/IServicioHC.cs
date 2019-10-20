8dce3a0a62f37a2e633776aa85890b7db6961d59
Efrain <efrain@sernasis.com>
2019-10-20 09:32:35 -0500

    Inicial
    Estructura vacía del proyecto.

---
+++ SernaSis.SernaSotomayor.WCF/Sernasis.SernaSotomayor.WCF.Contratos/IServicioHC.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IServicioHC
+{
+
+	[OperationContract]
+	string GetData(int value);
+
+	[OperationContract]
+	CompositeType GetDataUsingDataContract(CompositeType composite);
+
+	// TODO: agregue aquí sus operaciones de servicio
+}
+
+// Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
---
+++ SernaSis.SernaSotomayor.WCF/Sernasis.SernaSotomayor.WCF.Implement/ServicioHC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class ServicioHC : IServicioHC
+{
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
